bc3c0b3bf77f98e6fe270f0eb6a24ed8a69ef679
Travis Choma <travischoma@gmail.com>
2014-02-14 15:23:24 -0800

    trigger input event when input value is set

--- garlic.js
+++ garlic.js
@@ -165,10 +165,13 @@
 
         // for input[type=text], select and textarea, just set val()
         this.$element.val( storedValue );
+ 
+        // trigger an input event given the value has been changed
+        this.$element.trigger('input');
 
         // trigger custom user function when data is retrieved
         this.options.onRetrieve( this.$element, storedValue );
-
+        
         return;
       }
     }
